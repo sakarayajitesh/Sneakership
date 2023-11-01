23a1a26475f57ac00ae563f963ab171d7313ae81
Ajitesh Sakaray <sakarayajitesh@gmail.com>
2023-11-01 13:27:33 +0530

    Fix: Change Bottom Navigation Icons

--- app/src/main/java/com/ajitesh/sneakership/ui/MainScreen.kt
+++ app/src/main/java/com/ajitesh/sneakership/ui/MainScreen.kt
@@ -10,29 +10,29 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.ajitesh.sneakership.R
 import com.ajitesh.sneakership.ui.home.HomeScreen
 import com.ajitesh.sneakership.ui.saved.SavedScreen
 import com.ajitesh.sneakership.ui.search.SearchScreen
+import com.ajitesh.sneakership.ui.theme.Black
 
-sealed class BottomNavigationObject(val title: String, val icon: ImageVector) {
-    object Home : BottomNavigationObject(title = "home", icon = Icons.Outlined.Home)
-    object Search : BottomNavigationObject(title = "search", icon = Icons.Outlined.Search)
-    object Saved : BottomNavigationObject(title = "saved", icon = Icons.Outlined.Star)
+sealed class BottomNavigationObject(val title: String, val icon: Int) {
+    object Home : BottomNavigationObject(title = "home", icon = R.drawable.home_filled)
+    object Search : BottomNavigationObject(title = "search", icon = R.drawable.search_filled)
+    object Saved : BottomNavigationObject(title = "saved", icon = R.drawable.star_filled)
 }
 
 val bottomNavigationItems =
@@ -50,9 +50,11 @@ fun MainScreen(navigate: (String) -> Unit) {
             TopAppBar(
                 title = { Text(text = "SNEAKERSHIP", fontWeight = FontWeight.Medium) },
                 actions = {
-                    IconButton(onClick = {
-                        navigate("cart")
-                    }) {
+                    IconButton(
+                        onClick = {
+                            navigate("cart")
+                        }
+                    ) {
                         Icon(imageVector = Icons.Filled.ShoppingCart, contentDescription = "Cart")
                     }
                 }
@@ -67,10 +69,13 @@ fun MainScreen(navigate: (String) -> Unit) {
                             selectedItem = index
                             navController.navigate(s.title)
                         },
+                        selectedContentColor = Black,
+                        unselectedContentColor = Color.Gray,
                         icon = {
                             Icon(
-                                imageVector = s.icon,
-                                contentDescription = s.title
+                                painter = painterResource(s.icon),
+                                contentDescription = s.title,
+//                                modifier = Modifier.size(24.dp)
                             )
                         }
                     )
